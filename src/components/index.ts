c99053bfece332a9c56d41fc2c91152e84037071
Rui <rzhengphy@gmail.com>
2022-04-11 16:05:29 +0800

    export comparer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as ImageCarousel } from './ImageCarousel';
-// export { default as ImageTag } from './ImageTag';
-// export { default as PaginationBar } from './PaginationBar';
+export { default as ImageTag } from './ImageTag';
+export { default as PaginationBar } from './PaginationBar';
 export { default as ImageList } from './ImageList';
 export { default as StoreProvider } from './StoreProvider';
 export { default as ZAuth, getLambdaRunContext } from './Generic/auth';
+export { default as Comparer } from './Comparer';
