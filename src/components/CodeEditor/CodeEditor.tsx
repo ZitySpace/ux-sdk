455f3ec927dd93e329638e50d237102f3fed2ef3
Rui <rzhengphy@gmail.com>
2022-04-23 00:16:42 +0800

    add DataframeTable with paging

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -6,12 +6,81 @@ import {
   ViewGridIcon,
 } from '@heroicons/react/solid';
 
+import {
+  PagingStoreProvider,
+  createPagingStore,
+} from '../../stores/pagingStore';
+import PaginationBar from '../PaginationBar';
+
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-python';
 import 'ace-builds/src-noconflict/theme-monokai';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
-const DataFrame = ({ header, data }: { header: string[]; data: any[][] }) => {};
+const DataFrame = ({ header, data }: { header: string[]; data: any[][] }) => {
+  return (
+    <PagingStoreProvider createStore={createPagingStore}>
+      <div className='resize-y overflow-auto h-48 mt-1 bg-white'>
+        <table className='min-w-full divide-y divide-gray-200 relative table-fixed'>
+          <thead className='bg-gray-700 sticky top-0'>
+            <tr>
+              <th
+                scope='col'
+                className='px-3 py-2 text-left text-xs font-medium text-gray-200'
+                // onClick={}
+              >
+                <input
+                  type='checkbox'
+                  className='border-none focus:outline-none focus:ring-0 focus:ring-offset-0 bg-white w-3.5 h-3.5'
+                  // checked={}
+                  readOnly
+                />
+              </th>
+
+              {header.map((name, icol) => (
+                <th
+                  scope='col'
+                  key={icol}
+                  className='px-3 py-2 text-left text-xs font-medium text-gray-200'
+                >
+                  {name}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, irow) => (
+              <tr key={irow} className='bg-white border-b'>
+                <td className='px-3 py-2 whtiespace-nowrap'>
+                  <div className='flex items-center'>
+                    <input
+                      type='checkbox'
+                      className='border-none focus:outline-none focus:ring-0 focus:ring-offset-0 bg-indigo-100 w-3.5 h-3.5'
+                      // checked={}
+                      readOnly
+                    />
+                  </div>
+                </td>
+
+                {row.map((cell, icol) => (
+                  <td
+                    key={icol}
+                    className='px-3 py-2 whitespace-nowrap text-xs font-light text-gray-500 overflow-x-auto'
+                  >
+                    {cell}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className='sticky bottom-0 min-w-full'>
+          <PaginationBar />
+        </div>
+      </div>
+    </PagingStoreProvider>
+  );
+};
 
 const ACECodeEditor = ({
   title = 'Code Editor',
@@ -29,7 +98,9 @@ const ACECodeEditor = ({
   const code = useRef<string>(initCode);
 
   const [log, setLog] = useState<string>('');
-  const [result, setResult] = useState<string>('');
+  const [result, setResult] = useState<
+    { header: string[]; data: any[][] } | string
+  >('');
   const [showLog, setShowLog] = useState<boolean>(true);
   const [showResultPlain, setShowResultPlain] = useState<boolean>(true);
   const [showResultTable, setShowResultTable] = useState<boolean>(false);
@@ -47,7 +118,7 @@ const ACECodeEditor = ({
         );
         setResult(
           runResult && runResult.hasOwnProperty('result')
-            ? JSON.stringify(runResult.result, undefined, 4)
+            ? runResult.result
             : 'no result returned'
         );
       } catch (err) {
@@ -140,12 +211,18 @@ const ACECodeEditor = ({
       {showResultPlain && (
         <textarea
           className='resize-y overflow-auto h-48 mt-1 bg-gray-700 text-gray-200 text-xs px-4 pt-1 focus:outline-none'
-          value={result}
+          value={
+            typeof result === 'string'
+              ? result
+              : JSON.stringify(result, undefined, 4)
+          }
           placeholder='This displays the code run result'
           readOnly
         ></textarea>
       )}
-      {showResultTable && <></>}
+      {showResultTable && typeof result !== 'string' && (
+        <DataFrame header={result.header} data={result.data} />
+      )}
     </div>
   );
 };
