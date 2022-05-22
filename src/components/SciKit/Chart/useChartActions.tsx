ef3f36ac8d6771b0c43e4ee3ca9d8b154f1580aa
Rui <rzhengphy@gmail.com>
2022-05-22 23:39:36 +0800

    flat chart's element and background action settings in ChartEditorStory

--- src/components/SciKit/Chart/useChartActions.tsx
+++ src/components/SciKit/Chart/useChartActions.tsx
@@ -14,11 +14,6 @@ import 'ace-builds/src-noconflict/mode-python';
 import 'ace-builds/src-noconflict/theme-monokai';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
-enum ActionTarget {
-  Element = 'element',
-  Background = 'background',
-}
-
 const defaultElementAction = (params: EventParams) =>
   console.log(
     Object.fromEntries(
@@ -55,10 +50,6 @@ export const useChartActions = ({
     elementActions: ElementActionsProps;
     resetAction: ResetActionProps;
   }>(actionsInit);
-
-  const [actionTarget, setActionTarget] = useState<ActionTarget>(
-    ActionTarget.Element
-  );
 
   const setElementActions = (params: any) => {
     setActions({
@@ -122,85 +113,39 @@ export const useChartActions = ({
   };
 
   const Editor = (
-    <div>
-      <div className='flex jusity-start space-x-4'>
-        <button
-          type='button'
-          className={`w-20 inline-flex justify-center items-center px-3.5 py-2 border
-                        text-xs font-semibold rounded-lg shadow-sm
-                        hover:border-indigo-600 hover:ring-1 hover:ring-indigo-600
-                        ${
-                          actionTarget === 'element'
-                            ? 'text-gray-50 bg-indigo-600 outline-none'
-                            : 'text-gray-600 bg-transparent border-gray-400'
-                        }`}
-          onClick={() => {
-            setActionTarget(ActionTarget.Element);
-          }}
-        >
-          @element
-        </button>
-        <button
-          type='button'
-          className={`w-28 inline-flex justify-center items-center px-3.5 py-2 border
-                        text-xs font-semibold rounded-lg shadow-sm
-                        hover:border-indigo-600 hover:ring-1 hover:ring-indigo-600
-                        ${
-                          actionTarget === 'background'
-                            ? 'text-gray-50 bg-indigo-600 outline-none'
-                            : 'text-gray-600 bg-transparent border-gray-400'
-                        }`}
-          onClick={() => {
-            setActionTarget(ActionTarget.Background);
-          }}
-        >
-          @background
-        </button>
-      </div>
-      <div className='pt-2'>
+    <>
+      <div>
+        <div className='relative flex items-center w-full'>
+          <div className='flex-grow border-t border-indigo-200'></div>
+          <span className='flex-shrink mx-4 text-indigo-400 text-xs font-semibold'>
+            @element
+          </span>
+          <div className='flex-grow border-t border-indigo-200'></div>
+        </div>
         <div className='flex justify-start'>
-          {actionTarget === ActionTarget.Element ? (
-            <ScikitGroup
-              hideTitle={true}
-              hideFooter={true}
-              flat={true}
-              scroll={false}
-              yesCallback={setElementActions}
-              reactive={true}
-              key={0}
-            >
-              <Select
-                name='actionName'
-                options={[
-                  'click',
-                  'dblclick',
-                  'mousedown',
-                  'mousemove',
-                  'mouseup',
-                  'mouseover',
-                  'mouseout',
-                ]}
-                defaultValue='click'
-              />
-              <Input name='elementQuery' defaultValue='series' />
-            </ScikitGroup>
-          ) : (
-            <ScikitGroup
-              hideTitle={true}
-              hideFooter={true}
-              flat={true}
-              scroll={false}
-              yesCallback={setBackgroundAction}
-              reactive={true}
-              key={1}
-            >
-              <Select
-                name='actionName'
-                options={['click']}
-                defaultValue='click'
-              />
-            </ScikitGroup>
-          )}
+          <ScikitGroup
+            hideTitle={true}
+            hideFooter={true}
+            flat={true}
+            scroll={false}
+            yesCallback={setElementActions}
+            reactive={true}
+          >
+            <Select
+              name='actionName'
+              options={[
+                'click',
+                'dblclick',
+                'mousedown',
+                'mousemove',
+                'mouseup',
+                'mouseover',
+                'mouseout',
+              ]}
+              defaultValue='click'
+            />
+            <Input name='elementQuery' defaultValue='series' />
+          </ScikitGroup>
         </div>
 
         <div className='flex items-start justify-start -translate-y-1'>
@@ -219,12 +164,8 @@ export const useChartActions = ({
               name='Option'
               fontSize={14}
               readOnly={false}
-              value={
-                actionTarget === ActionTarget.Element
-                  ? elementActionQuery
-                  : backgroundActionQuery
-              }
-              placeholder='Write pandas DataFrame query here'
+              value={elementActionQuery}
+              placeholder='Write pandas DataFrame query for element action'
               editorProps={{ $blockScrolling: true }}
               setOptions={{
                 enableBasicAutocompletion: true,
@@ -235,16 +176,71 @@ export const useChartActions = ({
               maxLines={Infinity}
               width='100%'
               showPrintMargin={false}
-              onChange={
-                actionTarget === ActionTarget.Element
-                  ? updateElementActionQuery
-                  : updateBackgroundActionQuery
-              }
+              onChange={updateElementActionQuery}
             />
           </div>
         </div>
       </div>
-    </div>
+
+      <div>
+        <div className='relative flex items-center w-full'>
+          <div className='flex-grow border-t border-indigo-200'></div>
+          <span className='flex-shrink mx-4 text-indigo-400 text-xs font-semibold'>
+            @background
+          </span>
+          <div className='flex-grow border-t border-indigo-200'></div>
+        </div>
+        <div className='flex justify-start'>
+          <ScikitGroup
+            hideTitle={true}
+            hideFooter={true}
+            flat={true}
+            scroll={false}
+            yesCallback={setBackgroundAction}
+            reactive={true}
+          >
+            <Select
+              name='actionName'
+              options={['click']}
+              defaultValue='click'
+            />
+          </ScikitGroup>
+        </div>
+
+        <div className='flex items-start justify-start -translate-y-1'>
+          <div className='w-28 mr-3 relative h-7 flex-none'>
+            <label className='block absolute right-0 text-right text-sm font-medium text-gray-700'>
+              actionQuery
+            </label>
+          </div>
+          <div
+            className='resize-y overflow-auto h-12 pt-1 grow'
+            style={{ backgroundColor: '#272822' }}
+          >
+            <AceEditor
+              mode='typescript'
+              theme='monokai'
+              name='Option'
+              fontSize={14}
+              readOnly={false}
+              value={backgroundActionQuery}
+              placeholder='Write pandas DataFrame query for background query'
+              editorProps={{ $blockScrolling: true }}
+              setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: true,
+                enableSnippets: false,
+              }}
+              //   height='100%'
+              maxLines={Infinity}
+              width='100%'
+              showPrintMargin={false}
+              onChange={updateBackgroundActionQuery}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 
   return { actions, Editor };
