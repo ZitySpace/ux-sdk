1ae0001e88572b5802b5219bf7d7c7d818e1688e
Rui <rzhengphy@gmail.com>
2022-12-05 11:59:09 +0800

    fix: related tailwindcss utilities not generated for Comparer due to dynamic variable

--- src/components/Comparer/Comparer.tsx
+++ src/components/Comparer/Comparer.tsx
@@ -32,6 +32,10 @@ const Comparer = ({
     setStep(nItems);
   }, []);
 
+  // hack so tailwind generate these utilities cuz dynamic Components.length
+  // won't generate them unless in jit mode
+  const _ = ['grid-cols-2', 'grid-cols-3', 'grid-cols-4', 'grid-cols-5'];
+
   return (
     <div className='bg-gray-100 h-full flex flex-col text-xs shadow-lg rounded-md select-none'>
       <div className='bg-indigo-400 py-2 px-2 rounded-t-md flex justify-center space-x-2'>
